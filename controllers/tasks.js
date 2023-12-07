6c0cfdb9d616067f972b117409109087476a6562
estherab <esther.abenito@gmail.com>
2023-12-07 20:12:19 +0100

    Initial solution

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,22 @@
-// importar modelo
+import { TaskModel } from "../models/task"
 
 export const getAllTasks = (req, res) => {
-  const tasks = // ????
-  res.json(tasks)
+
+    const tasks = await TaskModel.getAllTasks()
+    res.json(tasks)
 }
 
 export const addTask = (req, res) => {
-  const task = // ????
+
+  const task = await TaskModel.addTask(req.body.description)
   res.json(task)
 }
 
 export const deleteTask = (req, res) => {
-  // ????
-  res.json({ message: 'Deleted successfully' })
+  try {
+    await TaskModel.deleteTask(req.params.id)
+    res.json({ message: 'Task deleted' })
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
--- models/task.js
+++ models/task.js
@@ -2,41 +2,64 @@
  * Array to store tasks.
  * @type {Array<Object>}
  */
-const tasks = []
+ const tasks = []
 
-/**
- * ID for the next task. Increments each time a new task is added.
- * @type {number}
- */
-let currentId = 1
-
-/**
- * Model class representing a task.
- * @class
- */
-
-/**
- * export class TaskModel {
- *
- *
-  /**
-   * Fetches all tasks.
-   * @name TaskModel.getAllTasks
-   * @static
-   * @returns {Array<Object>} Array of tasks.
-   */
-
-/**
-   * Adds a new task to the list.
-   * @name TaskModel.addTask
-   * @static
-   * @param {string} description - The description of the task.
-   * @returns {Object} The new task object.
-   */
-
-/**
-   * Deletes a task from the list by its ID.
-   * @name TaskModel.deleteTask
-   * @static
-   * @param {number} id - The ID of the task to delete.
-   */
+ /**
+  * ID for the next task. Increments each time a new task is added.
+  * @type {number}
+  */
+ let currentId = 1
+ 
+ /**
+  * Model class representing a task.
+  * @class
+  */
+ 
+ 
+ export class TaskModel {
+   /**
+    * Fetches all tasks.
+    * @name TaskModel.getAllTasks
+    * @static
+    * @returns {Array<Object>} Array of tasks.
+    */
+    static async getAllTasks() {
+       return tasks;
+    } 
+ /**
+    * Adds a new task to the list.
+    * @name TaskModel.addTask
+    * @static
+    * @param {string} description - The description of the task.
+    * @returns {Object} The new task object.
+    */
+    static async addTask(description) {
+       const task = {
+          id: currentId,
+          description
+       }
+       
+       tasks.push()
+       currentId++;
+ 
+       return task;
+    }
+ /**
+    * Deletes a task from the list by its ID.
+    * @name TaskModel.deleteTask
+    * @static
+    * @param {number} id - The ID of the task to delete.
+    */ 
+    static async(id) {
+       const idInt = parseInt(id, 10)
+ 
+       if (isNaN(idInt)) throw new Error('Invalid ID')
+ 
+       const index = tasks.findIndex(task => task.id = id)
+ 
+       if (index === -1) throw new Error('ID not found')
+       
+       tasks.splice(index, 1)
+    }
+ }
+ 
