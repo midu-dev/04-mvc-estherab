c3b6b24110ec61fd38be1ba56db94a3bd6e9dd89
estherab <esther.abenito@gmail.com>
2023-12-07 20:27:48 +0100

    Error fixed 3

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,22 +1,20 @@
-import { TaskModel } from "../models/task"
+// importar modelo
+import { TaskModel } from '../models/task.js'
 
-export const getAllTasks = (req, res) => {
-
-    const tasks = TaskModel.getAllTasks()
-    res.json(tasks)
+export const getAllTasks = async (req, res) => {
+  
+  const tasks = TaskModel.getAllTasks()
+  res.json(tasks)
 }
 
-export const addTask = (req, res) => {
+export const addTask = async (req, res) => {
 
   const task = TaskModel.addTask(req.body.description)
   res.json(task)
 }
 
 export const deleteTask = (req, res) => {
-  try {
-    TaskModel.deleteTask(req.params.id)
-    res.json({ message: 'Task deleted' })
-  } catch (error) {
-    res.status(500).json({ message: error.message })
-  }
+
+  TaskModel.deleteTask(req.params.id)
+  res.json({ message: 'Task deleted' })
 }
